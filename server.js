40db57810086e9d72f401b0dc1e6e3aaee33bc31
BassaniDavid <davbassan98@gmail.com>
2025-03-27 14:38:23 +0100

    feat: exercise complete

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 2000
+const postsRouter = require('./routers/posts')
+
+app.use('/posts', postsRouter)
 
 app.listen(port, () => {
     console.log('server in ascolto alla porta ' + port)
