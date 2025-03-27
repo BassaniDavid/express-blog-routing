2ea92e2fe5a320ace6e1336f27987c5ee34faccd
BassaniDavid <davbassan98@gmail.com>
2025-03-27 14:44:12 +0100

    feat: first bonus

--- routers/posts.js
+++ routers/posts.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const router = express.Router()
+const posts = require('../postsArray')
 
 // index
 router.get('/', (req, res) => {
-    res.send('Lista dei post')
+    res.json(posts)
+
 })
 
 // show
